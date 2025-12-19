a151678aa2deb87c0d2d9e37a7a2a3fe21f40cb8
kmanikon <103885541+kmanikon@users.noreply.github.com>
2025-12-19 04:47:38 -0600

    added session caching

--- src/api/eonet.js
+++ src/api/eonet.js
@@ -6,10 +6,17 @@ export async function fetchEvents({
   status = "open",
 } = {}) {
 try {
+    if (sessionStorage.getItem("eonet_data") != null) {
+        const eonet_json = JSON.parse(sessionStorage.getItem("eonet_data"))
+        if (eonet_json?.events && eonet_json?.events?.length > 0) {
+            return eonet_json?.events
+        }
+    }
     const res = await fetch(
         `${BASE_URL}/events?days=${days}&limit=${limit}&status=${status}`
     );
     const data = await res.json();
+    sessionStorage.setItem("eonet_data", JSON.stringify(data))
     return data.events;
 } catch (e) {
     console.log("error fetching nasa eonet api...")
